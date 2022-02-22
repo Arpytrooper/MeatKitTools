5181160a508c1bb3337b1098d2688288b9280a1a
Arpytrooper <90275734+Arpytrooper@users.noreply.github.com>
2022-02-21 22:30:16 -0500

    Update OIDAutoComplete.cs
    Made it work with item spawner entries

--- Assets/MeatKit/Tools/IDTools/editor/OIDAutoComplete.cs
+++ Assets/MeatKit/Tools/IDTools/editor/OIDAutoComplete.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using FistVR;
+using OtherLoader;
 
 
 [CreateAssetMenu(fileName = "OIDAutoComplete", menuName = "Object IDs/AutoComplete", order = 1)]
 public class OIDAutoComplete : ScriptableObject
 {
+    
 
     public FVRObject.ObjectCategory Category;
     public AppendType typeOfAppend = OIDAutoComplete.AppendType.No_Custom_String;
@@ -32,11 +34,20 @@ public class OIDAutoComplete : ScriptableObject
     // public bool CreateAtObjectLocation;
     public bool WillSpawnInTakeAndHold = true;
     //[Tooltip("This is the path that the FVRObjectID will be created at if you do not have 'CreateAtObjectLocation' checked")]
-   
+    [Header("Spawner Entry Stuff")]
+    public bool UsesCustomPath = false;
+    public string CustomPath;
+    public ItemSpawnerV2.PageMode MainCategory;
+    public ItemSpawnerID.ESubCategory SubCategory;
+    public bool IsDisplayedInMainEntry = true;
+    public List<string> ModTags;
+    public List<string> TutorialBlockIds;
+    public bool UsesLargeSpawnPad = false;
+    public bool IsReward = false;
     [ContextMenu("AutoFillItemIDs")]
     public void AutoFillItemIDs()
     {
-        Debug.Log("pressed");
+        //Debug.Log("pressed");
         char lastCharOfExportLocation;
         bool isExportLocationValid = false;
         lastCharOfExportLocation = ExportLocation[ExportLocation.Length - 1];               
@@ -101,6 +112,97 @@ public class OIDAutoComplete : ScriptableObject
                 {
                     IDObject.OSple = false;
                 }
+            }
+
+
+        }
+    }
+    [ContextMenu("FillIDsANDEntries")]
+    public void FillIDsANDEntries()
+    {
+        //Debug.Log("pressed");
+        char lastCharOfExportLocation;
+        bool isExportLocationValid = false;
+        lastCharOfExportLocation = ExportLocation[ExportLocation.Length - 1];
+        if (lastCharOfExportLocation == '/')
+        {
+            isExportLocationValid = true;
+        }
+        else
+        {
+            isExportLocationValid = false;
+            Debug.Log("You need to end your file path with a /");
+        }
+        if (ExportLocation != null && isExportLocationValid)
+        {
+            foreach (GameObject fillOut in ObjectsToID)
+            {
+                string assetPath = AssetDatabase.GetAssetPath(fillOut);
+
+                //string Path = AssetDatabase.GetAssetPath(fillOut) + "OID " + fillOut.name;
+                string Path = ExportLocation + "OID " + fillOut.name + ".asset";
+                string EntryPath = ExportLocation + "ISE " + fillOut.name + ".asset";
+                // if (!CreateAtObjectLocation)
+                // {
+                //      Path = ExportLocation + "OID " + fillOut.name;
+                //  }
+                FVRObject IDObject = ScriptableObject.CreateInstance<FVRObject>();
+                AssetDatabase.CreateAsset(IDObject, Path);
+                string itemID = fillOut.name;
+                if (typeOfAppend == AppendType.Suffix)
+                {
+                    itemID = fillOut.name + "." + customAppend;
+                }
+                else if (typeOfAppend == AppendType.Prefix)
+                {
+                    itemID = customAppend + "." + fillOut.name;
+                }
+
+                IDObject.m_anvilPrefab.AssetName = assetPath;
+                IDObject.Category = Category;
+                IDObject.ItemID = itemID;
+                IDObject.SpawnedFromId = itemID;
+                IDObject.DisplayName = fillOut.name;
+                IDObject.TagEra = Era;
+                IDObject.TagSet = Set; /*
+            IDObject.TagFirearmSize = FirearmSize;
+            IDObject.TagFirearmAction = FirearmAction;
+            IDObject.TagFirearmRoundPower = RoundPower;
+            IDObject.TagFirearmFirstYear = FirstYear;
+            IDObject.TagFirearmFiringModes = firingMode;
+            IDObject.TagFirearmFeedOption = FeedOptions;
+            IDObject.TagFirearmMounts = FirearmMounts; */
+                IDObject.TagAttachmentMount = AttachmentMounts;
+                IDObject.TagAttachmentFeature = AttachmentFeature;
+                /* IDObject.TagMeleeStyle =
+                   IDObject.TagPowerupType =
+                   IDObject.TagThrownType =
+                   IDObject.TagThrownDamageType = 
+                   IDObject.RoundType = */
+                if (WillSpawnInTakeAndHold)
+                {
+                    IDObject.OSple = true;
+                }
+                else
+                {
+                    IDObject.OSple = false;
+                }
+                ItemSpawnerEntry EntryObject = ScriptableObject.CreateInstance<ItemSpawnerEntry>();
+                AssetDatabase.CreateAsset(EntryObject, EntryPath);
+                EntryObject.MainObjectID = itemID;
+                if (UsesCustomPath)
+                {
+                    EntryObject.EntryPath = CustomPath;
+                } else
+                {
+                    EntryObject.Page = MainCategory;
+                    EntryObject.SubCategory = SubCategory;
+                }
+                EntryObject.DisplayName = fillOut.name;
+                EntryObject.ModTags = ModTags;
+                EntryObject.TutorialBlockIDs = TutorialBlockIds;
+                EntryObject.UsesLargeSpawnPad = UsesLargeSpawnPad;
+                EntryObject.IsReward = IsReward;
             }
 
 
